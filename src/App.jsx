cd2d02587dfd5b8965d20b8fe4174e5207b33db7
AmanSamaiya <amansamaiya90@gmail.com>
2024-03-17 01:41:48 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-     <h1 className=' text-lg text-cyan-800 font-bold text-center'>Hello</h1>
+     <Footer/>
     </>
   )
 }
