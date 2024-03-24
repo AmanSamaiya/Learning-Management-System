740c39760b1f249ecdb8e040537473336e0eab58
AmanSamaiya <amansamaiya90@gmail.com>
2024-03-25 00:38:05 +0530

    added logout functionality

--- src/Layouts/Homelayout.jsx
+++ src/Layouts/Homelayout.jsx
@@ -3,27 +3,23 @@ import Footer from "../components/Footer.jsx";
 import { FiMenu } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../redux/slices/authSlice.js";
 
 function HomeLayout({ children }) {
-
-
-
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
 
-  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn)
-  const role = useSelector((state) => state?.auth?.role)
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+  const role = useSelector((state) => state?.auth?.role);
 
-
-  function onLogOut(e){
-
+  async function onLogOut(e) {
     e.preventDefault();
-    
-
-    navigate("/")
+    const response = await dispatch(logout());
+    if (response?.payload?.data) {
+      navigate("/");
+    }
   }
-
 
   return (
     <>
@@ -33,75 +29,73 @@ function HomeLayout({ children }) {
 
           <div className="drawer-content">
             {/* Page content here */}
-            <label
-              htmlFor="my-drawer"
-              className="drawer-button"
-            >
+            <label htmlFor="my-drawer" className="drawer-button">
               <FiMenu size={"36px"} className="font-bold text-white m-4" />
             </label>
           </div>
 
           <div className="drawer-side">
-            <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
             <ul className="menu p-4 w-80 min-h-full bg-base-200  text-white text-lg">
               {/* Sidebar content here */}
               <li>
-                <Link to="/" > Home </Link>
+                <Link to="/"> Home </Link>
               </li>
 
               {isLoggedIn && role === "ADMIN" && (
-                  <li>
-                    <Link to = "/admin/dashboard"> Admin Dashboard</Link>
-                  </li>
+                <li>
+                  <Link to="/admin/dashboard"> Admin Dashboard</Link>
+                </li>
               )}
 
-
-
               <li>
-                <Link to="/about" > About Us </Link>
+                <Link to="/about"> About Us </Link>
               </li>
               <li>
-                <Link to="/contact" > Contact Us </Link>
+                <Link to="/contact"> Contact Us </Link>
               </li>
               <li>
-                <Link to="/courses" > All Courses </Link>
+                <Link to="/courses"> All Courses </Link>
               </li>
-
 
               {!isLoggedIn ? (
                 <li className=" ml-14 w-[60%]">
-                    <div className=" w-full flex items-center justify-center">
-                        <button className=" btn btn-primary px-4 py-1 font-semibold rounded-md w-full">
-                              <Link to = "/login">Login</Link>
-                          </button>
-                        <button className=" btn btn-secondary px-4 py-1 font-semibold rounded-md w-full">
-                              <Link to = "/login">Signup</Link>
-                          </button>
-                    
-                    </div>
+                  <div className=" w-full flex items-center justify-center">
+                    <Link
+                      to="/signin"
+                      className=" btn btn-primary px-4 py-1 font-semibold rounded-md w-full"
+                    >
+                      <button>Login</button>
+                    </Link>
+
+                    <Link
+                      to="/signup"
+                      className=" btn btn-secondary px-4 py-1 font-semibold rounded-md w-full"
+                    >
+                      <button>Signup</button>
+                    </Link>
+                  </div>
                 </li>
               ) : (
                 <li className=" ml-14 w-[60%]">
-                    <div className=" w-full flex items-center justify-center">
-                        <button className=" btn btn-primary px-4 py-1 font-semibold rounded-md w-full">
-                              <Link to = "/user/profile">Profile</Link>
-                          </button>
-                        <button className=" btn btn-secondary px-4 py-1 font-semibold rounded-md w-full">
-                              <Link onClick={onLogOut}>Logout</Link>
-                          </button>
-                    
-                    </div>
+                  <div className=" w-full flex items-center justify-center">
+                    <button className=" btn btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                      <Link to="/user/profile">Profile</Link>
+                    </button>
+                    <button className=" btn btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                      <Link onClick={onLogOut}>Logout</Link>
+                    </button>
+                  </div>
                 </li>
-              )
-            
-            }
-
-
+              )}
             </ul>
           </div>
         </div>
       </div>
-
 
       {children}
       <Footer />
